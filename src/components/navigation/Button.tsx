4d56e6482efa308cdf4407db57c3a6c21b47ae3a
yash373 <yash.yasodhan.123@gmail.com>
2024-06-08 17:41:01 +0530

    added redirect for home page

---
+++ src/components/navigation/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface ButtonProps {
+    link: string,
+    color: boolean
+}
+
+const Button = ({link , color}: ButtonProps) => {
+  return (
+    <div>
+      button
+    </div>
+  )
+}
+
+export default Button
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 const Home = () => {
+  const router = useRouter()
+
+  useEffect(()=>{
+    router.push("/navigation")
+  })
+
   return (
     <div>
       home page
