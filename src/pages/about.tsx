7bae6d600f9fcd3a71dae964121186ab395d63cb
yash373 <yash.yasodhan.123@gmail.com>
2024-06-09 07:17:07 +0530

    removed comments from about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,12 +6,9 @@ import Text from '@/components/about/Text'
 const About = () => {
   return (
     <div className='flex h-[100vh] w-[95%] md:overflow-clip overflow-y-scroll justify-center items-center flex-col'>
-      {/* Logo */}
       <Logo />
       <div className='md:space-x-5 sm:space-y-5 md:mb-10 flex flex-col md:flex-row my-auto'>
-        {/* Random Image */}
         <Image className='my-auto mx-auto md:w-[50%] rounded-md' src={"/farmer.jpg"} width={500} height={500} alt='farmer-image' />
-        {/* why its good */}
         <Text />
       </div>
     </div>
