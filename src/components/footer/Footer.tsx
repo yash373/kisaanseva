4acda4159c788793afc011d496e96e9a43883d2a
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-10 20:12:27 +0530

    backgrond color changes

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 const Footer = () => {
     return (
-        <Link href={"/navigation"} className='w-full mt-auto h-[10vh] bottom-0 fixed flex bg-slate-700'>
+        <Link href={"/navigation"} className='w-full mt-auto h-[10vh] bottom-0 fixed flex bg-slate-900'>
             <p className='text-center mx-auto my-auto text-xl'>
                 <IoMdHome />
             </p>
