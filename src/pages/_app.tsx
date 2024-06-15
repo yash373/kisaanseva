5a2e2861dd96ab1db41dba72bb81c403281019aa
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-15 17:26:02 +0530

    fixed mobile errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import Footer from "@/components/footer/Footer";
 import { atom } from "jotai";
 import Sidebar from "@/components/sidebar/Sidebar";
 import { BrowserView } from "react-device-detect";
+import { useEffect, useState } from "react";
 
 const kart = atom<ItemProps[]>([])
 
@@ -19,13 +20,30 @@ export interface ItemProps {
 }
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [isPC, setIsPC] = useState(false)
+
+  const detectDeviceType = () =>
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+      ? false
+      : /(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(
+        navigator.userAgent
+      )
+        ? false
+        : true;
+
+  useEffect(()=>{
+    if (detectDeviceType()) {
+      setIsPC(true)
+    }
+  })
+
   return (
     <div className="flex flex-col bg-slate-950 text-white justify-center items-center h-full">
       <div className="flex w-full h-[90vh] overflow-y-auto">
         <Component {...pageProps} />
-        <BrowserView>
-          <Sidebar />
-        </BrowserView>
+        {isPC && <Sidebar />}
       </div>
       <Footer />
     </div>
