7f95e31d638e843e998fc552f94ca37f3a06d952
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-15 15:09:18 +0530

    got sidebar working only for laptop

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import Footer from "@/components/footer/Footer";
 import { atom } from "jotai";
 import Sidebar from "@/components/sidebar/Sidebar";
+import { BrowserView } from "react-device-detect";
 
 const kart = atom<ItemProps[]>([])
 
@@ -22,7 +23,9 @@ export default function App({ Component, pageProps }: AppProps) {
     <div className="flex flex-col bg-slate-950 text-white justify-center items-center h-full">
       <div className="flex w-full h-[90vh] overflow-y-auto">
         <Component {...pageProps} />
-        <Sidebar />
+        <BrowserView>
+          <Sidebar />
+        </BrowserView>
       </div>
       <Footer />
     </div>
