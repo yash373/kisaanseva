a5f01e2a28bd1a413b6ce382c2b77e293445926d
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 21:20:58 +0530

    added heading for connect to transport

--- src/pages/connecttotransport.tsx
+++ src/pages/connecttotransport.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Association from '@/components/connecttomarket/Association'
+import Heading from '@/components/explore/Heading'
 
 const ConnectToTransport = () => {
   return (
     <div className='flex flex-col space-y-2 w-full'>
+      <Heading text='Connect To Transport 🚚' />
       <Association name='Associated Road Carriers Limited' desc="Sudhama Nagar, 1St Cross Road, Near Ghatge Patil Transports, Bangalore, Karnataka 560027" link='https://www.arclimited.com/' />
       <Association name='Sharma Transports' desc="Door No -81, Next To Amba Bajaj Show Room, After Ayyappa Temple, Hosur Main Road, Madiwala, Zuzuvadi, 1St Stage, Btm Layout, Bengaluru, Karnataka 560068" link='http://www.sharmatransports.com/' />
       <Association name='Sharma Transports' desc="No. 139/4, 1St Block, 1St Main, Dr Rajkumar Road, Near Allahabad Bank, Rajajinagar, E Block, 2Nd Stage, Rajajinagar, Bengaluru, Karnataka 560010" link='http://www.sharmatransports.com/' />
