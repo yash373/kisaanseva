62f4c9fc30c8a3585f990319343a3bdea2438318
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 17:10:16 +0530

    got pm-kmy scheme

--- src/pages/governmentschemes.tsx
+++ src/pages/governmentschemes.tsx
@@ -3,8 +3,9 @@ import Scheme from '@/components/governmentschemes/Scheme'
 
 const GovernmentSchemes = () => {
   return (
-    <div className='flex flex-col w-full'>
-      <Scheme link='https://www.enam.gov.in/web/' name='E-NAM' desc="An online trading platform for agricultural communities in India" />
+    <div className='flex flex-col space-y-2 w-full'>
+      <Scheme link='https://www.enam.gov.in/web/' name='E-NAM' desc="an online trading platform for agricultural communities in India" />
+      <Scheme link='https://pmkisan.gov.in/' name='PM-KMY' desc="a pension scheme for small and marginal farmers" />
     </div>
   )
 }
