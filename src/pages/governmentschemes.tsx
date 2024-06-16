e093a871177ce0bfe223ec3ae29a6ed33637db88
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 17:14:44 +0530

    got pmksy

--- src/pages/governmentschemes.tsx
+++ src/pages/governmentschemes.tsx
@@ -8,6 +8,7 @@ const GovernmentSchemes = () => {
       <Scheme link='https://pmkisan.gov.in/' name='Pradhan Mantri Kisan Maan-Dhan Yojana (PM-KMY)' desc="a pension scheme for small and marginal farmers" />
       <Scheme link='https://pmfby.gov.in/' name='Pradhan Mantri Fasal Bima Yojana (PMFBY)' desc="a crop insurance scheme to protect farmers from natural calamities" />
       <Scheme link='https://www.myscheme.gov.in/schemes/kcc' name='Kisan Credit Card (KCC) Scheme' desc="a credit scheme to provide timely and adequate credit to farmers" />
+      <Scheme link='https://www.pmksy.gov.in/' name='Pradhan Mantri Krishi Sinchai Yojana (PMKSY)' desc="a scheme to enhance irrigation coverage and water use efficiency" />
     </div>
   )
 }
