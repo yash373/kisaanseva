2303bbf86f0ef8b00975ed8fd4647ebe41b28976
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 17:08:28 +0530

    created scheme item

---
+++ src/components/governmentschemes/Scheme.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'next/link'
+
+interface SchemeProps {
+    name: string,
+    link: string,
+    desc: string,
+}
+
+const Scheme = ({ name, desc, link }: SchemeProps) => {
+    return (
+        <Link target='_blank' href={link} className='border-2 w-full -ml-2 h-[5vh] rounded-md border-black flex text-center space-x-2 hover:text-green-500'>
+            <p className='ml-2 font-semibold'>{name}:</p>
+            <p className='font-light'>{desc}</p>
+        </Link>
+    )
+}
+
+export default Scheme
--- src/pages/governmentschemes.tsx
+++ src/pages/governmentschemes.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Scheme from '@/components/governmentschemes/Scheme'
 
 const GovernmentSchemes = () => {
   return (
-    <div>
-      government schemes
+    <div className='flex flex-col w-full'>
+      <Scheme link='https://www.enam.gov.in/web/' name='E-NAM' desc="An online trading platform for agricultural communities in India" />
     </div>
   )
 }
