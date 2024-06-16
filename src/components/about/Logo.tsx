7913496bbe84e7ac285f129dd1d8203cfadb5c8a
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 21:29:14 +0530

    changed logo to text instead of png as it is more easier to resize and manage

--- src/components/about/Logo.tsx
+++ src/components/about/Logo.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Logo = () => {
     return (
-        <img className='mx-auto md:-mt-14 md:-mb-10 h-32' src={"/OnlyFarmers.png"} alt='logo' />
+        // <img className='mx-auto md:-mt-14 md:-mb-10 h-32' src={"/OnlyFarmers.png"} alt='logo' />
+        <p className='font-extrabold text-3xl mb-4'>OnlyFarmers 👩‍🌾</p>
     )
 }
 
