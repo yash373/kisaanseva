c4b64aeca6a746a6489d564066df5dbf04e5720b
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 17:11:04 +0530

    name update

--- src/pages/governmentschemes.tsx
+++ src/pages/governmentschemes.tsx
@@ -5,6 +5,7 @@ const GovernmentSchemes = () => {
   return (
     <div className='flex flex-col space-y-2 w-full'>
       <Scheme link='https://www.enam.gov.in/web/' name='E-NAM' desc="an online trading platform for agricultural communities in India" />
+      <Scheme link='https://pmkisan.gov.in/' name='Pradhan Mantri Kisan Maan-Dhan Yojana (PM-KMY)' desc="a pension scheme for small and marginal farmers" />
       <Scheme link='https://pmkisan.gov.in/' name='PM-KMY' desc="a pension scheme for small and marginal farmers" />
     </div>
   )
