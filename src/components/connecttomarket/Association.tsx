d724f85a61f0eafc9d6f72760ebf05b28ea317ce
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 21:50:28 +0530

    updated association component so that it can be integrated into government schemes component

--- src/components/connecttomarket/Association.tsx
+++ src/components/connecttomarket/Association.tsx
@@ -5,13 +5,13 @@ interface AssociationProps {
     name: string,
     link: string,
     desc: string,
-    image: string
+    img: string
 }
 
-const Association = ({ name, desc="", link, image }: AssociationProps) => {
+const Association = ({ name, desc="", link, img }: AssociationProps) => {
     return (
         <Link target='_blank' href={link} className='border-2 h-[65vh] my-2 flex-col w-full md:w-1/4 rounded-md border-black flex hover:text-green-500'>
-            <img className='mx-auto text-xl h-[50vh] w-[50vw]' src={image} alt='schemeimg' />
+            <img className='mx-auto text-xl h-[50vh] w-[50vw]' src={img} alt='schemeimg' />
             <div className='flex flex-col space-x-2 text-lg'>
                 <p className='ml-2 font-semibold'>{name}:</p>
                 <p className='font-light'>{desc}</p>
