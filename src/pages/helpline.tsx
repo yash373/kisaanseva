130899b9cd41b43985ec496f7752e4b568af336f
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 21:47:11 +0530

    updated helpline page to work with new association component layout

--- src/pages/helpline.tsx
+++ src/pages/helpline.tsx
@@ -6,11 +6,13 @@ const Helpline = () => {
   return (
     <div className='flex flex-col space-y-2 w-full'>
       <Heading text='Get Help 🔴' />
-      <Association name='Uttar Pradesh' desc='2237617' link='tel:2237617' />
-      <Association name='West Bengal' desc='2252-0619' link='tel:22520619' />
-      <Association name='Madhya Pradesh' desc='1800 180 1551' link='tel:18001801551' />
-      <Association name='Karnataka' desc='18004 253553' link='tel:18004253553' />
-      <Association name='Maharashtra' desc='022-61316429' link='tel:02261316429' />
+      <div className='flex flex-row space-x-1 flex-wrap justify-evenly items-center'>
+        <Association name='Uttar Pradesh' desc='2237617' link='tel:2237617' />
+        <Association name='West Bengal' desc='2252-0619' link='tel:22520619' />
+        <Association name='Madhya Pradesh' desc='1800 180 1551' link='tel:18001801551' />
+        <Association name='Karnataka' desc='18004 253553' link='tel:18004253553' />
+        <Association name='Maharashtra' desc='022-61316429' link='tel:02261316429' />
+      </div>
     </div>
   )
 }
