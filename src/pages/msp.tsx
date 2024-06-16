5e9d485e84d6ce7d54d26c9da6d22ddc31906043
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 23:03:34 +0530

    updated msp page

--- src/pages/msp.tsx
+++ src/pages/msp.tsx
@@ -5,12 +5,12 @@ import { useEffect } from 'react'
 const MSP = () => {
     const router = useRouter()
 
-    useEffect(()=>{
+    const reroute = () => {
         router.push("https://pib.gov.in/PressReleaseIframePage.aspx?PRID=2003184")
-    })
+    }
     
     return (
-        <div>
+        <div className=''>
             you are currently being redirected to another website
         </div>
     )
