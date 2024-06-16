1e56db7aab5d08c4a1bc36cb4333cb3ddd67eb66
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 18:53:31 +0530

    got MP

--- src/pages/helpline.tsx
+++ src/pages/helpline.tsx
@@ -6,6 +6,7 @@ const Helpline = () => {
     <div className='flex flex-col space-y-2 w-full'>
       <Association name='Uttar Pradesh' desc='2237617' link='tel:2237617' />
       <Association name='West Bengal' desc='2252-0619' link='tel:22520619' />
+      <Association name='Madhya Pradesh' desc='1800 180 1551' link='tel:18001801551' />
     </div>
   )
 }
