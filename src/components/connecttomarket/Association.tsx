fbaabd89f54a50d5a7d7297b7516663cd90b370c
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 21:45:31 +0530

    updated association component to look more like scheme component

--- src/components/connecttomarket/Association.tsx
+++ src/components/connecttomarket/Association.tsx
@@ -5,13 +5,17 @@ interface AssociationProps {
     name: string,
     link: string,
     desc: string,
+    image: string
 }
 
-const Association = ({ name, desc="", link }: AssociationProps) => {
+const Association = ({ name, desc="", link, image }: AssociationProps) => {
     return (
-        <Link target='_blank' href={link} className='text-left border-2 w-full -ml-2 h-full rounded-md border-black flex space-x-2 hover:text-green-500'>
-            <p className='ml-2 font-semibold'>{name}</p>
-            <p className='font-light'>{desc}</p>
+        <Link target='_blank' href={link} className='border-2 h-[65vh] my-2 flex-col w-full md:w-1/4 rounded-md border-black flex hover:text-green-500'>
+            <img className='mx-auto text-xl h-[50vh] w-[50vw]' src={image} alt='schemeimg' />
+            <div className='flex flex-col space-x-2 text-lg'>
+                <p className='ml-2 font-semibold'>{name}:</p>
+                <p className='font-light'>{desc}</p>
+            </div>
         </Link>
     )
 }
