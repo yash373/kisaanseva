84429630c87e64804f4b052868d1a149f8f82b22
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 21:25:28 +0530

    added heading for get help page

--- src/pages/helpline.tsx
+++ src/pages/helpline.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Association from '@/components/connecttomarket/Association'
+import Heading from '@/components/explore/Heading'
 
 const Helpline = () => {
   return (
     <div className='flex flex-col space-y-2 w-full'>
+      <Heading text='Get Help 🔴' />
       <Association name='Uttar Pradesh' desc='2237617' link='tel:2237617' />
       <Association name='West Bengal' desc='2252-0619' link='tel:22520619' />
       <Association name='Madhya Pradesh' desc='1800 180 1551' link='tel:18001801551' />
