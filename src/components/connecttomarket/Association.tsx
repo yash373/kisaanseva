2aabd5107e3cdb39133bd90dacf1f8f0d52d6366
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 17:45:58 +0530

    fixed height issues for Scheme and Association component

--- src/components/connecttomarket/Association.tsx
+++ src/components/connecttomarket/Association.tsx
@@ -9,7 +9,7 @@ interface AssociationProps {
 
 const Association = ({ name, desc, link }: AssociationProps) => {
     return (
-        <Link target='_blank' href={link} className='border-2 w-full -ml-2 h-[5vh] rounded-md border-black flex text-center space-x-2 hover:text-green-500'>
+        <Link target='_blank' href={link} className='border-2 w-full -ml-2 h-full md:h-[5vh] rounded-md border-black flex text-center space-x-2 hover:text-green-500'>
             <p className='ml-2 font-semibold'>{name}</p>
             <p className='font-light'>{desc}</p>
         </Link>
