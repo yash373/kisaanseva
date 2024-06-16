7dedc1d706e99038f76d2aa43874b0174f77feae
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 18:25:42 +0530

    got 4 insurance companies

--- src/pages/insurancecompanies.tsx
+++ src/pages/insurancecompanies.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Association from '@/components/connecttomarket/Association'
 
 const InsuranceCompanies = () => {
   return (
-    <div>
-      connect to insurance companies
+    <div className='flex flex-col space-y-2 w-full'>
+      <Association name='Pradhan Mantri Fasal Bima Yojana (PMFBY)' link='https://pmfby.gov.in/' desc="" />
+      <Association name='Weather based Crop Insurance Scheme (WBCIS)' link='https://www.india.gov.in/weather-based-crop-insurance-scheme-wbcis' desc="" />
+      <Association name='Coconut Palm insurance scheme (CPIS)' link='https://www.myscheme.gov.in/schemes/cpis' desc="" />
+      <Association name='Pilot Unified Package insurance scheme (UPIS) (45 districts)' link='https://vikaspedia.in/agriculture/agri-insurance/unified-package-insurance-scheme' desc="" />
     </div>
   )
 }
