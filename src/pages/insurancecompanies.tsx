cd8fac1eaaa0feec25164dd6df92bddcea0f01dd
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 18:36:35 +0530

    got lic

--- src/pages/insurancecompanies.tsx
+++ src/pages/insurancecompanies.tsx
@@ -8,6 +8,7 @@ const InsuranceCompanies = () => {
       <Association name='Weather based Crop Insurance Scheme (WBCIS)' link='https://www.india.gov.in/weather-based-crop-insurance-scheme-wbcis' desc="" />
       <Association name='Coconut Palm insurance scheme (CPIS)' link='https://www.myscheme.gov.in/schemes/cpis' desc="" />
       <Association name='Pilot Unified Package insurance scheme (UPIS) (45 districts)' link='https://vikaspedia.in/agriculture/agri-insurance/unified-package-insurance-scheme' desc="" />
+      <Association name='LIC Of India' link='https://licindia.in/' desc="" />
     </div>
   )
 }
