504b87c0c5dbd6bad78826a6723049f110e954d1
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 17:44:36 +0530

    got 1 association

--- src/pages/connecttomarket.tsx
+++ src/pages/connecttomarket.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Scheme from '@/components/governmentschemes/Scheme'
+import Association from '@/components/connecttomarket/Association'
 
 const ConnectToMarket = () => {
   return (
     <div className='flex flex-col space-y-2 w-full'>
-      connect to market
+      <Association name='National Agricultural Co Op Marketing Fed Of India Limited' desc="Niton, 11/3, Bengaluru (Bangalore) Urban, Palace Road, Bengaluru, Karnataka 560052" link='https://www.nafed-india.com/' />
     </div>
   )
 }
