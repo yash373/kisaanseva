099a039d77596c637884d9099bd8492a077703b6
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 21:21:55 +0530

    added heading for government schemes

--- src/pages/governmentschemes.tsx
+++ src/pages/governmentschemes.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Scheme from '@/components/governmentschemes/Scheme'
+import Heading from '@/components/explore/Heading'
 
 const GovernmentSchemes = () => {
   return (
     <div className='flex flex-col space-y-4 w-full'>
+      <Heading text='Government Schemes 💼' />
       <Scheme img='/schemes/enam.jpeg' link='https://www.enam.gov.in/web/' name='E-NAM' desc="an online trading platform for agricultural communities in India" />
       <Scheme img='/schemes/pmkmy.jpeg' link='https://pmkisan.gov.in/' name='Pradhan Mantri Kisan Maan-Dhan Yojana (PM-KMY)' desc="a pension scheme for small and marginal farmers" />
       <Scheme img='/schemes/pmfby.jpeg' link='https://pmfby.gov.in/' name='Pradhan Mantri Fasal Bima Yojana (PMFBY)' desc="a crop insurance scheme to protect farmers from natural calamities" />
