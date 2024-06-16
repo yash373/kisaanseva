87136431d95115dca643d94762dc6a8bfd4e14ca
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 21:48:05 +0530

    updated insurance schemes page to work with association component

--- src/pages/insurancecompanies.tsx
+++ src/pages/insurancecompanies.tsx
@@ -6,11 +6,13 @@ const InsuranceCompanies = () => {
   return (
     <div className='flex flex-col space-y-2 w-full'>
       <Heading text='Insurance Schemes 🛡' />
-      <Association name='Pradhan Mantri Fasal Bima Yojana (PMFBY)' link='https://pmfby.gov.in/' desc="" />
-      <Association name='Weather based Crop Insurance Scheme (WBCIS)' link='https://www.india.gov.in/weather-based-crop-insurance-scheme-wbcis' desc="" />
-      <Association name='Coconut Palm insurance scheme (CPIS)' link='https://www.myscheme.gov.in/schemes/cpis' desc="" />
-      <Association name='Pilot Unified Package insurance scheme (UPIS) (45 districts)' link='https://vikaspedia.in/agriculture/agri-insurance/unified-package-insurance-scheme' desc="" />
-      <Association name='LIC Of India' link='https://licindia.in/' desc="" />
+      <div className='flex flex-row space-x-1 flex-wrap justify-evenly items-center'>
+        <Association name='Pradhan Mantri Fasal Bima Yojana (PMFBY)' link='https://pmfby.gov.in/' desc="" />
+        <Association name='Weather based Crop Insurance Scheme (WBCIS)' link='https://www.india.gov.in/weather-based-crop-insurance-scheme-wbcis' desc="" />
+        <Association name='Coconut Palm insurance scheme (CPIS)' link='https://www.myscheme.gov.in/schemes/cpis' desc="" />
+        <Association name='Pilot Unified Package insurance scheme (UPIS) (45 districts)' link='https://vikaspedia.in/agriculture/agri-insurance/unified-package-insurance-scheme' desc="" />
+        <Association name='LIC Of India' link='https://licindia.in/' desc="" />
+      </div>
     </div>
   )
 }
