5963e130345ca80c20ec629d8d399b158fc4ee57
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 19:29:07 +0530

    improved navbar look

--- src/components/about/Logo.tsx
+++ src/components/about/Logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 const Logo = () => {
     return (
-        <img className='mx-auto md:-mb-12 h-32' src={"/OnlyFarmers.png"} alt='logo' />
+        <img className='mx-auto md:-mt-14 md:-mb-10 h-32' src={"/OnlyFarmers.png"} alt='logo' />
     )
 }
 
