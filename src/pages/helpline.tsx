fb5700595e8206041e4fb992ae9eac9a6a1d794b
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 18:54:26 +0530

    got karnataka helpline number

--- src/pages/helpline.tsx
+++ src/pages/helpline.tsx
@@ -7,6 +7,7 @@ const Helpline = () => {
       <Association name='Uttar Pradesh' desc='2237617' link='tel:2237617' />
       <Association name='West Bengal' desc='2252-0619' link='tel:22520619' />
       <Association name='Madhya Pradesh' desc='1800 180 1551' link='tel:18001801551' />
+      <Association name='Karnataka' desc='18004 253553' link='tel:18004253553' />
     </div>
   )
 }
