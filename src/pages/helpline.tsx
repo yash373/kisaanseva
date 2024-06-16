fded41cee932d4a2e338e4e75537859a3cc2510b
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 18:51:18 +0530

    got wb helpline number

--- src/pages/helpline.tsx
+++ src/pages/helpline.tsx
@@ -5,6 +5,7 @@ const Helpline = () => {
   return (
     <div className='flex flex-col space-y-2 w-full'>
       <Association name='Uttar Pradesh' desc='2237617' link='tel:2237617' />
+      <Association name='West Bengal' desc='2252-0619' link='tel:22520619' />
     </div>
   )
 }
