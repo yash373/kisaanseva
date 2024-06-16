188e00332edc4b2a31d227f13aead0b77332641e
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 21:38:11 +0530

    updated government schemes page and scheme component

--- src/components/governmentschemes/Scheme.tsx
+++ src/components/governmentschemes/Scheme.tsx
@@ -10,9 +10,9 @@ interface SchemeProps {
 
 const Scheme = ({ name, desc, link, img }: SchemeProps) => {
     return (
-        <Link target='_blank' href={link} className='border-2 flex-col w-full -ml-2 h-full rounded-md border-black flex hover:text-green-500'>
+        <Link target='_blank' href={link} className='border-2 h-[65vh] my-2 flex-col w-full md:w-1/4 rounded-md border-black flex hover:text-green-500'>
             <img className='mx-auto text-xl h-[50vh] w-[50vw]' src={img} alt='schemeimg' />
-            <div className='flex space-x-2 text-lg'>
+            <div className='flex flex-col space-x-2 text-lg'>
                 <p className='ml-2 font-semibold'>{name}:</p>
                 <p className='font-light'>{desc}</p>
             </div>
