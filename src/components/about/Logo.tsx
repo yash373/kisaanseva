8fcc8a838f4bc545f5c4274fc593b79a2f0cce2d
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 22:45:26 +0530

    optimized logo

--- src/components/about/Logo.tsx
+++ src/components/about/Logo.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Logo = () => {
     return (
+        <div className='flex w-full space-x-2'>
+            <p className='font-extrabold text-4xl mb-4 uppercase underline'>OnlyFarmers 👩‍🌾</p>
+            <p className='text-md font-semibold mt-auto'> - Empowering Futures, Cultivating Futures.</p>
+        </div>
         // <img className='mx-auto md:-mt-14 md:-mb-10 h-32' src={"/OnlyFarmers.png"} alt='logo' />
-        <p className='font-extrabold text-3xl mb-4'>OnlyFarmers 👩‍🌾</p>
     )
 }
 
