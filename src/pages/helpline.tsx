c79dde06fedca3c3beea6e4a233e8658ef7d6c66
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 18:46:42 +0530

    created helpline page

---
+++ src/pages/helpline.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Association from '@/components/connecttomarket/Association'
+
+const Helpline = () => {
+  return (
+    <div className='flex flex-col space-y-2 w-full'>
+      <Association name='Uttar Pradesh' desc='2237617' link='tel:2237617' />
+    </div>
+  )
+}
+
+export default Helpline
