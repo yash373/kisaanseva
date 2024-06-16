dcce4c3446a56d7ded8ff4c2f68ade2b92e3e428
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 17:47:05 +0530

    got 2 associations

--- src/pages/connecttomarket.tsx
+++ src/pages/connecttomarket.tsx
@@ -5,6 +5,7 @@ const ConnectToMarket = () => {
   return (
     <div className='flex flex-col space-y-2 w-full'>
       <Association name='National Agricultural Co Op Marketing Fed Of India Limited' desc="Niton, 11/3, Bengaluru (Bangalore) Urban, Palace Road, Bengaluru, Karnataka 560052" link='https://www.nafed-india.com/' />
+      <Association name='Institution Of Agricultural Technologists' desc="No-15, Queens Road, Near-Sanjay Vani, Vasanth Nagar, Bengaluru, Karnataka 560052" link='http://www.iatbangalore.com/' />
     </div>
   )
 }
