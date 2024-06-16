28803541f4b80920daa77fa774635be716a940ba
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-16 17:12:18 +0530

    got pmfby

--- src/pages/governmentschemes.tsx
+++ src/pages/governmentschemes.tsx
@@ -6,7 +6,7 @@ const GovernmentSchemes = () => {
     <div className='flex flex-col space-y-2 w-full'>
       <Scheme link='https://www.enam.gov.in/web/' name='E-NAM' desc="an online trading platform for agricultural communities in India" />
       <Scheme link='https://pmkisan.gov.in/' name='Pradhan Mantri Kisan Maan-Dhan Yojana (PM-KMY)' desc="a pension scheme for small and marginal farmers" />
-      <Scheme link='https://pmkisan.gov.in/' name='PM-KMY' desc="a pension scheme for small and marginal farmers" />
+      <Scheme link='https://pmfby.gov.in/' name='Pradhan Mantri Fasal Bima Yojana (PMFBY)' desc="a crop insurance scheme to protect farmers from natural calamities" />
     </div>
   )
 }
