e58001ef751f7114788661ea7b0e37cadc8444ec
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-17 09:34:32 +0530

    got centralized footer back

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,7 +6,7 @@ import { FaRegCopyright } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div className='flex justify-start space-x-5 md:flex-wrap flex-col md:flex-row border-t-4 h-[10vh] border-black'>
+        <div className='flex justify-center space-x-5 md:flex-wrap flex-col md:flex-row border-t-4 h-[10vh] border-black'>
             <FooterLink plain={false} title='+91-11233889919' icon={<p className='font-bold'>Nation Agriculture Helpline Number: </p>} link='tel:9111233889919' />
             <FooterLink link='' plain={true} title='Yashodhan Mandal' icon={<FaRegCopyright />}  />
             <FooterLink link='' plain={true} title='NHCE, Bengaluru' icon={<FaMapMarkerAlt />}  />
