e477f8e267cfeed31c9db8be3a8ac54d090bd9b7
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-17 08:09:05 +0530

    creatd footer link component

---
+++ src/components/footer/FooterLink.tsx
@@ -0,0 +1,17 @@
+import React, { ReactElement } from 'react'
+
+interface FooterLinkProps {
+    title: string,
+    link: string,
+    icon: ReactElement
+}
+
+const FooterLink = ({title, link, icon}: FooterLinkProps) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FooterLink
