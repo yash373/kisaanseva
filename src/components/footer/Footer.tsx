0e5a151d08a6f17e50e0e863bfab266e3c0702fa
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-17 08:39:14 +0530

    added national agriculture help line number

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,6 +6,7 @@ import { FaCode } from "react-icons/fa";
 const Footer = () => {
     return (
         <div className='flex space-x-5 md:flex-wrap flex-col md:flex-row border-t-4 h-[10vh] border-black'>
+            <FooterLink plain={false} title='+91-11233889919' icon={<p className='font-bold'>Nation Agriculture Helpline Number: </p>} link='tel:9111233889919' />
             <FooterLink plain={false} title='Repository' icon={<FaGithub />} link='https://github.com/yash373/onlyfarmers' />
             <FooterLink link='' plain={true} title='Yashodhan Mandal' icon={<FaCode />}  />
         </div>
