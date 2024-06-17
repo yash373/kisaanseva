b422d08c434f17f7403ba1a65de3df28b763ae62
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-17 08:27:55 +0530

    added plain switch in footer link component to accomodate other links in future

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ import { FaGithub } from "react-icons/fa";
 const Footer = () => {
   return (
     <div className='flex md:flex-wrap flex-col md:flex-row border-t-4 h-[10vh] border-black'>
-      <FooterLink title='Repository' icon={<FaGithub />} link='https://github.com/yash373/onlyfarmers' />
+      <FooterLink plain={false} title='Repository' icon={<FaGithub />} link='https://github.com/yash373/onlyfarmers' />
     </div>
   )
 }
--- src/components/footer/FooterLink.tsx
+++ src/components/footer/FooterLink.tsx
@@ -4,18 +4,27 @@ import Link from 'next/link'
 interface FooterLinkProps {
     title: string,
     link: string,
+    plain: boolean,
     icon: ReactElement
 }
 
-const FooterLink = ({title, link, icon}: FooterLinkProps) => {
-  return (
-    <Link className='flex justify-center items-center space-x-1 hover:text-green-500 font-semibold' href={link}>
-      {icon}
-      <div>
-        {title}
-      </div>
-    </Link>
-  )
+const FooterLink = ({ title, link = "", plain = false, icon }: FooterLinkProps) => {
+    return (
+        <>
+            {!plain && <Link className='flex justify-center items-center space-x-1 hover:text-green-500 font-semibold' href={link}>
+                {icon}
+                <div>
+                    {title}
+                </div>
+            </Link>}
+            {plain && <div className='flex justify-center items-center space-x-1 hover:text-green-500 font-semibold'>
+                {icon}
+                <div>
+                    {title}
+                </div>
+            </div>}
+        </>
+    )
 }
 
 export default FooterLink
