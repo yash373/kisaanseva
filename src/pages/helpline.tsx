8b8866f2d14518142c1dca1d27b09de8c923eec1
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-17 09:09:10 +0530

    made maharashtra help line number more descriptive

--- src/pages/helpline.tsx
+++ src/pages/helpline.tsx
@@ -11,7 +11,7 @@ const Helpline = () => {
         <Association big={false} img='https://img.etimg.com/thumb/msid-58513140,width-672,resizemode-4,imgsize-133246/news/economy/agriculture/west-bengal-starts-organic-farming-activities-across-the-state/farming-bccl.jpg' name='West Bengal Agriculture Helpline Number' desc='2252-0619' link='tel:22520619' />
         <Association big={false} img='https://thumbs.dreamstime.com/z/dewas-madhya-pradesh-india-farmer-using-oxen-working-field-indian-farming-scene-soybean-plants-dewas-madhya-pradesh-157197053.jpg' name='Madhya Pradesh Agriculture Helpline Number' desc='1800 180 1551' link='tel:18001801551' />
         <Association big={false} img='https://www.alightindia.com/CDN/Content/Images/PostImages/ORIGINAL/kr3--523ad0.jpg' name='Karnataka Agriculture Helpline Number' desc='18004 253553' link='tel:18004253553' />
-        <Association big={false} img='https://newsd.in/wp-content/uploads/2016/10/maharashtr.jpg' name='Maharashtra' desc='022-61316429' link='tel:02261316429' />
+        <Association big={false} img='https://newsd.in/wp-content/uploads/2016/10/maharashtr.jpg' name='Maharashtra Agriculture Helpline Number' desc='022-61316429' link='tel:02261316429' />
       </div>
     </div>
   )
