ff6996ab5d9c1f441deb653b6ea7a36eee503d72
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-17 08:17:39 +0530

    imported footer link into footer and improvised on css

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import FooterLink from './FooterLink'
 
 const Footer = () => {
   return (
-    <div className='flex border-t-4 h-[10vh] border-black'>
+    <div className='flex md:flex-wrap flex-col md:flex-row border-t-4 h-[10vh] border-black'>
       footer
     </div>
   )
