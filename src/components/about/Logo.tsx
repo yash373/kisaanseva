98e71177fc2e7db81550e903a735f723a8d8d507
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-17 09:18:58 +0530

    name change

--- src/components/about/Logo.tsx
+++ src/components/about/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Logo = () => {
     return (
         <div className='flex w-full border-b-4 border-black justify-center items-center space-x-2'>
-            <p className='font-extrabold text-5xl mb-4 uppercase'>OnlyFarmers 👩‍🌾</p>
+            <p className='font-extrabold text-5xl mb-4 uppercase'>KisaanSeva 👩‍🌾</p>
             <p className='text-md font-semibold mt-auto'> - Empowering Futures, Cultivating Futures.</p>
         </div>
         // <img className='mx-auto md:-mt-14 md:-mb-10 h-32' src={"/OnlyFarmers.png"} alt='logo' />
