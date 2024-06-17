c0d04d093280d748d5724a59d297bf6319ebf984
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-17 08:25:26 +0530

    Got github link in footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import FooterLink from './FooterLink'
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className='flex md:flex-wrap flex-col md:flex-row border-t-4 h-[10vh] border-black'>
-      footer
+      <FooterLink title='Repository' icon={<FaGithub />} link='https://github.com/yash373/onlyfarmers' />
     </div>
   )
 }
