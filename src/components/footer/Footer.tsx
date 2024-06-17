b59b5ed4449bd949efc73ba98c4b2419f5496b58
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-17 08:32:56 +0530

    added developer details to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import FooterLink from './FooterLink'
 import { FaGithub } from "react-icons/fa";
+import { FaCode } from "react-icons/fa";
 
 const Footer = () => {
-  return (
-    <div className='flex md:flex-wrap flex-col md:flex-row border-t-4 h-[10vh] border-black'>
-      <FooterLink plain={false} title='Repository' icon={<FaGithub />} link='https://github.com/yash373/onlyfarmers' />
-    </div>
-  )
+    return (
+        <div className='flex space-x-5 md:flex-wrap flex-col md:flex-row border-t-4 h-[10vh] border-black'>
+            <FooterLink plain={false} title='Repository' icon={<FaGithub />} link='https://github.com/yash373/onlyfarmers' />
+            <FooterLink link='' plain={true} title='Yashodhan Mandal' icon={<FaCode />}  />
+        </div>
+    )
 }
 
 export default Footer
