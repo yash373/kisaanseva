2b2b5c0149e7e7d558e36aab6daa827bcfaf689f
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-17 11:50:35 +0530

    fixed homepage display on mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,9 @@ import Text from '@/components/about/Text'
 
 const Home = () => {
   return (
-    <div className='flex justify-center items-center flex-col'>
-      {/* <Logo /> */}
-      <div className='md:space-x-5 sm:space-y-5 md:mb-10 flex flex-col md:flex-row my-auto'>
-        <Image className='my-auto mx-auto md:w-[50%] rounded-md' src={"/farmer.jpg"} width={500} height={500} alt='farmer-image' />
-        <Text />
-      </div>
+    <div className='flex flex-col md:flex-row md:space-x-5 h-full w-full'>
+      <Image className='my-auto mx-auto md:w-[50%] rounded-md' src={"/farmer.jpg"} width={500} height={500} alt='farmer-image' />
+      <Text />
     </div>
   )
 }
