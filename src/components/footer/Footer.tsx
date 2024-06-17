09c665db0ca1b160d00999f08d11431fc0756a7d
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-17 08:48:54 +0530

    improved css of footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,11 +6,11 @@ import { FaMapMarkerAlt } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div className='flex space-x-5 md:flex-wrap flex-col md:flex-row border-t-4 h-[10vh] border-black'>
+        <div className='flex justify-start space-x-5 md:flex-wrap flex-col md:flex-row border-t-4 h-[10vh] border-black'>
             <FooterLink plain={false} title='+91-11233889919' icon={<p className='font-bold'>Nation Agriculture Helpline Number: </p>} link='tel:9111233889919' />
-            <FooterLink plain={false} title='Repository' icon={<FaGithub />} link='https://github.com/yash373/onlyfarmers' />
             <FooterLink link='' plain={true} title='Yashodhan Mandal' icon={<FaCode />}  />
             <FooterLink link='' plain={true} title='NHCE, Bengaluru' icon={<FaMapMarkerAlt />}  />
+            <FooterLink plain={false} title='Repository' icon={<FaGithub />} link='https://github.com/yash373/onlyfarmers' />
         </div>
     )
 }
