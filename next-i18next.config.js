c840a6d8da002790d0f00d9440b2923ba8a26ee1
yash373 <yash.yasodhan.123gmail.com>
2024-07-15 19:29:10 +0530

    Revert "took a few steps back"
    This reverts commit 5f94b423ec78c906f01484e8b5a66b85da5c14e1.

---
+++ next-i18next.config.js
@@ -0,0 +1,7 @@
+// next-i18next.config.js
+module.exports = {
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en", "mr", "hi", "pa", "sa", "bho", "ur", "gu", "te", "bn", "kn", "ta", "ml"]
+    }
+}
