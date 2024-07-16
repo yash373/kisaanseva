e3aebf332a33c692b8b04d21125b1e0707a737da
yash373 <yash.yasodhan.123gmail.com>
2024-07-16 18:16:22 +0530

    created search state

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Search = () => {
+  const [search, setSearch] = useState<string>("")
+
   return (
     <div>
       this is the search box
