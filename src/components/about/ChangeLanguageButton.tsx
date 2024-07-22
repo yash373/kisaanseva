8a872287b9e096f18ffefff30302c94713b0043f
yash373 <yash.yasodhan.123gmail.com>
2024-07-22 18:42:15 +0530

    made text centered for change language button

--- src/components/about/ChangeLanguageButton.tsx
+++ src/components/about/ChangeLanguageButton.tsx
@@ -5,8 +5,10 @@ const ChangeLanguageButton = () => {
     const link = "https://support.google.com/chrome/answer/173424?hl=en&co=GENIE.Platform%3DDesktop#:~:text=1%20On%20your%20computer%2C%20open%20Chrome.%202%20Go,highlighted%20text.%205%20Click%20Translate%20selection%20to%20%5BLanguage%5D."
 
     return (
-        <Link className='md:w-[15%] mr-2 my-auto flex mx-auto m-2 md:p-2 p-4 hover:bg-green-700 bg-green-400 rounded-md font-semibold' href={link} target='_blank' >
+        <Link className='md:w-[15%] mr-2 my-auto flex mx-auto m-2 md:p-2 p-4 hover:bg-green-700 bg-green-400 rounded-md' href={link} target='_blank' >
+            <p className='font-semibold mx-auto'>
                 Change Language 👆
+            </p>
         </Link>
     )
 }
