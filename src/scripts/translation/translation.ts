04e6cf4ab1194a124ee7824d9811fe8ea83d5384
yash373 <yash.yasodhan.123gmail.com>
2024-07-26 00:04:35 +0530

    made translation function only work if language is not english to save money in api costs

--- src/scripts/translation/translation.ts
+++ src/scripts/translation/translation.ts
@@ -3,26 +3,31 @@ const translate = async (targetLanguage: string, text: string) => {
 
     const url = 'https://deep-translate1.p.rapidapi.com/language/translate/v2';
 
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'x-rapidapi-key': '2010442decmshc62de9e7247e8e1p15b96cjsn1cf015d3184b',
-                'x-rapidapi-host': 'deep-translate1.p.rapidapi.com',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                q: text,
-                source,
-                target: targetLanguage
-            })
-        });
-        const result = await response.json();
-        return (result["data"]["translations"]["translatedText"]);
-    } catch (error) {
-        console.log("An Error Occured")
-        console.error(error);
+    if (targetLanguage != "en") {
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'x-rapidapi-key': '2010442decmshc62de9e7247e8e1p15b96cjsn1cf015d3184b',
+                    'x-rapidapi-host': 'deep-translate1.p.rapidapi.com',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    q: text,
+                    source,
+                    target: targetLanguage
+                })
+            });
+            const result = await response.json();
+            return (result["data"]["translations"]["translatedText"]);
+        } catch (error) {
+            console.log("An Error Occured")
+            console.error(error);
+        }
+    }else{
+        return text
     }
+
 }
 
 translate("fr","hello")
