80e7cc6fadac9daadeedef77992b43c6466fe78f
yash373 <yash.yasodhan.123gmail.com>
2024-07-25 21:56:45 +0530

    wrote handle close function for language component

--- src/components/languageselection/Language.tsx
+++ src/components/languageselection/Language.tsx
@@ -1,17 +1,25 @@
 import React from 'react'
+import { useAtom } from 'jotai'
+import { openLanguageSelector } from '@/pages/_app'
 
 interface LanguageProps {
     lang: string,
     code: string,
-    changeLanguage : (code:string) => void
+    changeLanguage: (code: string) => void,
 }
 
-const Language = ({lang, code, changeLanguage}: LanguageProps) => {
-  return (
-    <div>
-        
-    </div>
-  )
+const Language = ({ lang, code, changeLanguage }: LanguageProps) => {
+    const [openSelector, setOpenSelector] = useAtom(openLanguageSelector)
+
+    const handleClose = () => {
+        setOpenSelector(false)
+    }
+
+    return (
+        <div>
+
+        </div>
+    )
 }
 
 export default Language
