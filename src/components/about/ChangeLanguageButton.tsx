10879176d22ce2b741d0f7ed6a2260b06a1e6852
yash373 <yash.yasodhan.123gmail.com>
2024-07-25 22:51:53 +0530

    made change language button wire to open language selector when clicked

--- src/components/about/ChangeLanguageButton.tsx
+++ src/components/about/ChangeLanguageButton.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import Link from 'next/link'
+import { useAtom } from 'jotai'
+import { openLanguageSelector } from '@/pages/_app'
 
 const ChangeLanguageButton = () => {
-    const link = "https://support.google.com/chrome/answer/173424?hl=en&co=GENIE.Platform%3DDesktop#:~:text=1%20On%20your%20computer%2C%20open%20Chrome.%202%20Go,highlighted%20text.%205%20Click%20Translate%20selection%20to%20%5BLanguage%5D."
+    // const link = "https://support.google.com/chrome/answer/173424?hl=en&co=GENIE.Platform%3DDesktop#:~:text=1%20On%20your%20computer%2C%20open%20Chrome.%202%20Go,highlighted%20text.%205%20Click%20Translate%20selection%20to%20%5BLanguage%5D."
+
+    const [openSelector, setOpenSelector] = useAtom(openLanguageSelector)
 
     return (
-        <Link className='md:w-[15%] mr-2 my-auto flex mx-auto m-2 md:p-2 p-4 hover:bg-green-700 bg-green-400 rounded-md' href={link} target='_blank' >
+        <button onClick={()=>{setOpenSelector(true)}}  className='md:w-[15%] mr-2 my-auto flex mx-auto m-2 md:p-2 p-4 hover:bg-green-700 bg-green-400 rounded-md' >
             <p className='font-semibold mx-auto'>
                 Change Language 👆
             </p>
-        </Link>
+        </button>
     )
 }
 
