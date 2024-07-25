4ae02aaff7c2946d21c4f9cfb1ec0651787194f0
yash373 <yash.yasodhan.123gmail.com>
2024-07-25 23:22:43 +0530

    added punjabi

--- src/components/languageselection/LanguageSelector.tsx
+++ src/components/languageselection/LanguageSelector.tsx
@@ -34,6 +34,7 @@ const LanguageSelector = ({  }: LanguageSelectorProps) => {
             <Language lang='Marathi' code='mr' changeLanguage={changeLanguage} />
             <Language lang='Nepali' code='ne' changeLanguage={changeLanguage} />
             <Language lang='Oriya' code='or' changeLanguage={changeLanguage} />
+            <Language lang='Punjabi' code='pa' changeLanguage={changeLanguage} />
         </div>
     )
 }
