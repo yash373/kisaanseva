e78ddf84dd82e742d2df905b6b82a096dcc9872d
yash373 <yash.yasodhan.123gmail.com>
2024-07-25 22:00:39 +0530

    turned on language selector on temporoarily for dev

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ import LanguageSelector from "@/components/languageselection/LanguageSelector";
 
 const kart = atom<ItemProps[]>([])
 const lang = atom<string>("en")
-const openLanguageSelector = atom<boolean>(false)
+const openLanguageSelector = atom<boolean>(true)
 
 export { kart, lang, openLanguageSelector }
 
