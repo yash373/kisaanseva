d6b13163b0e00b8c5ec9bfc78464ffbd05aff2d1
yash373 <yash.yasodhan.123gmail.com>
2024-07-25 21:40:38 +0530

    wrote properties for language component

--- src/components/languageselection/Language.tsx
+++ src/components/languageselection/Language.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
-const Language = () => {
+interface LanguageProps {
+    lang: string,
+    code: string,
+    changeLanguage : (code:string) => void
+}
+
+const Language = ({lang, code, changeLanguage}: LanguageProps) => {
   return (
     <div>
       
