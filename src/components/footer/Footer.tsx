eb1e6f038e8385fd8593206016f0b1c87b3b31b5
yash373 <yash.yasodhan.123gmail.com>
2024-07-26 01:11:22 +0530

    changed icon in footer for making more concise and more impactful

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,11 +3,12 @@ import FooterLink from './FooterLink'
 import { FaGithub } from "react-icons/fa";
 import { FaMapMarkerAlt } from "react-icons/fa";
 import { FaRegCopyright } from "react-icons/fa";
+import { GiSuicide } from "react-icons/gi";
 
 const Footer = () => {
     return (
-        <div className='flex justify-center space-x-5 md:flex-wrap flex-col md:flex-row border-t-4 h-full border-black'>
-            <FooterLink plain={false} title='+91-11233889919' icon={<p className='font-bold'>Nation Agriculture Helpline Number: </p>} link='tel:9111233889919' />
+        <div className='flex justify-start space-x-7 md:flex-wrap flex-col md:flex-row border-t-4 h-full border-black'>
+            <FooterLink plain={true} title='+91-11233889919' icon={<GiSuicide />} link='tel:9111233889919' />
             <FooterLink link='' plain={true} title='Yashodhan Mandal' icon={<FaRegCopyright />}  />
             <FooterLink link='' plain={true} title='NHCE, Bengaluru' icon={<FaMapMarkerAlt />}  />
             <FooterLink plain={false} title='Repository' icon={<FaGithub />} link='https://github.com/yash373/kisaansevaa' />
