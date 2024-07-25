65beaed3ff19e7f78908cf0b2071938a928174d2
yash373 <yash.yasodhan.123gmail.com>
2024-07-25 23:15:43 +0530

    changed api for more languages availibility

--- src/scripts/translation/translation.ts
+++ src/scripts/translation/translation.ts
@@ -1,30 +1,30 @@
 const translate = async (targetLanguage: string, text: string) => {
     const source = "en"
 
-    const url = `https://microsoft-translator-text-api3.p.rapidapi.com/translate?to=${targetLanguage}&from=${source}&textType=plain`;
+    const url = 'https://deep-translate1.p.rapidapi.com/language/translate/v2';
 
     try {
         const response = await fetch(url, {
-            method:"POST",
+            method: 'POST',
             headers: {
                 'x-rapidapi-key': '2010442decmshc62de9e7247e8e1p15b96cjsn1cf015d3184b',
-                'x-rapidapi-host': 'microsoft-translator-text-api3.p.rapidapi.com',
+                'x-rapidapi-host': 'deep-translate1.p.rapidapi.com',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify([
-                {
-                    text:text
-                }
-            ])
+            body: JSON.stringify({
+                q: text,
+                source,
+                target: targetLanguage
+            })
         });
-        const result = await response.text();
-        console.log(result);
+        const result = await response.json();
+        return (result["data"]["translations"]["translatedText"]);
     } catch (error) {
         console.log("An Error Occured")
         console.error(error);
     }
 }
 
-// translate("fr","hello")
+translate("fr","hello")
 
 export { translate }
