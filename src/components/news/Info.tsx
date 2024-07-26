aa185f921ac0eaf9a4ee4f228145968b614c4d7b
yash373 <yash.yasodhan.123gmail.com>
2024-07-26 11:21:01 +0530

    wrote info component props

---
+++ src/components/news/Info.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface InfoProps {
+    img: string,
+    title: string,
+    para: string
+}
+
+const Info = ({img, title, para}: InfoProps) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Info
