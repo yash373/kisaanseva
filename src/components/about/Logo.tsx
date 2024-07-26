f8878177a526d5f35936277b6c3b8a3fb7c6c4ed
yash373 <yash.yasodhan.123gmail.com>
2024-07-26 09:44:16 +0530

    made logo text centered

--- src/components/about/Logo.tsx
+++ src/components/about/Logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 const Logo = () => {
     return (
-        <Link href="/"  className='flex w-full md:w-[25%] justify-center items-center space-x-2'>
+        <Link href="/"  className='flex w-full text-center mx-auto md:w-[25%] justify-center items-center space-x-2'>
             <p className='font-extrabold w-full text-4xl mb-4 uppercase'>KisaanSeva 👩‍🌾</p>
             {/* <p className='text-md w-full font-semibold mt-auto'> - Empowering Futures, Cultivating Futures.</p> */}
         </Link>
