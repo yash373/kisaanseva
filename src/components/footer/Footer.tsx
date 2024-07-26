c10427cba30e07afc3dfe336574bb1c308a62b8b
yash373 <yash.yasodhan.123gmail.com>
2024-07-26 11:31:12 +0530

    added more spacing in between footer links

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,7 +7,7 @@ import { GiSuicide } from "react-icons/gi";
 
 const Footer = () => {
     return (
-        <div className='flex justify-start space-x-7 md:flex-wrap flex-col md:flex-row border-t-4 h-full border-black'>
+        <div className='flex justify-start md:space-y-0 space-y-2 md:space-x-7 md:flex-wrap flex-col md:flex-row border-t-4 h-full border-black'>
             <FooterLink plain={true} title='+91-11233889919' icon={<GiSuicide />} link='tel:9111233889919' />
             <FooterLink link='' plain={true} title='Yashodhan Mandal' icon={<FaRegCopyright />}  />
             <FooterLink link='' plain={true} title='NHCE, Bengaluru' icon={<FaMapMarkerAlt />}  />
