88bcb7612b2d31ff386597bee3d189f7b755a036
yash373 <yash.yasodhan.123gmail.com>
2024-07-26 11:23:31 +0530

    wrote info component

--- src/components/news/Info.tsx
+++ src/components/news/Info.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
+import Heading from '../languageselection/Heading'
+import Link from 'next/link'
 
 interface InfoProps {
     img: string,
     title: string,
-    para: string
+    para: string,
+    link: string
 }
 
-const Info = ({img, title, para}: InfoProps) => {
-  return (
-    <div>
-      
-    </div>
-  )
+const Info = ({ img, title, para, link }: InfoProps) => {
+    return (
+        <Link href={link} target='_blank' className='flex flex-col mx-auto w-[70vw] h-full'>
+            <img src={img} alt="info" />
+            <Heading text={title} />
+            <p>{para}</p>
+        </Link>
+    )
 }
 
 export default Info
