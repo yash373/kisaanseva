4eb8a16b0e738567db63d1e6cdb6d6baff7e5359
yash373 <yash.yasodhan.123gmail.com>
2024-07-26 14:39:27 +0530

    fixed speeling error on index page

--- src/components/about/Text.tsx
+++ src/components/about/Text.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Text = () => {
     return (
         <div className='flex flex-col h-full my-auto w-[95%] mx-auto md:w-1/2 space-y-2 justify-center items-center'>
-            <p className='font-extrabold text-3xl text-center'>How we help farmers 👩‍🌾?</p>
+            <p className='font-extrabold text-3xl text-center'>How do we help farmers 👩‍🌾?</p>
             <p className='text-left text-wrap'>Imagine a robust web application designed exclusively for farmers, offering a comprehensive array of tools and resources tailored to their needs. This innovative platform seamlessly integrates agricultural schemes, cutting-edge farming tools, and a diverse selection of high-quality seeds. Farmers can effortlessly browse through detailed information on various agricultural schemes, access up-to-date market trends, and connect with agricultural experts for personalized advice. The application features a user-friendly interface that allows farmers to manage their operations efficiently, from planning and planting to harvesting and marketing. With its extensive database of agricultural products and services, this web application becomes the indispensable digital companion for every farmer, empowering them to optimize productivity and make informed decisions in today&apos;s dynamic farming landscape.</p>
         </div>
     )
