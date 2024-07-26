665a3aa0d5d66262f0741c369e001cf09b77be4c
yash373 <yash.yasodhan.123gmail.com>
2024-07-26 12:15:36 +0530

    made text even more smaller

--- src/components/product/ProductText.tsx
+++ src/components/product/ProductText.tsx
@@ -13,7 +13,7 @@ const ProductText = ({ title, desc, price, item }: ProductTextProps) => {
     return (
         <div className='flex flex-col md:py-0 py-4 space-y-3'>
             <p className='text2xl md:text-2xl font-extrabold capitalize'>{title}</p>
-            <p className='text-lg font-semibold'>{desc}</p>
+            <p className='font-semibold'>{desc}</p>
             <p className='text-lg font-bold'>₹ {price}</p>
             <ProductKartButton item={item} />
         </div>
