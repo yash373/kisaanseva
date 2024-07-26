f6173d0087f38404e765f2b7c760ca8263a28c41
yash373 <yash.yasodhan.123gmail.com>
2024-07-26 14:20:55 +0530

    fixed page component button bug

--- src/components/reroute/Page.tsx
+++ src/components/reroute/Page.tsx
@@ -12,7 +12,7 @@ const Page = ({ img, title, operation }: PageProps) => {
             <img className='h-[50vh] w-[70vw] rounded-md mx-auto' src={img} />
             <div className='flex md:flex-row flex-col space-y-3 md:space-y-0 mx-auto md:space-x-5'>
                 <p className='font-extrabold text-xl mx-auto my-auto'>{title}</p>
-                <button onClick={()=>{operation()}} className='flex text-center md:p-2 p-4 bg-green-400 mx-auto hover:bg-green-700 rounded-md font-semibold'>
+                <button onClick={()=>{operation()}} className='flex text-center md:p-2 p-4 bg-green-400 mx-auto w-[80%] md:w-auto hover:bg-green-700 rounded-md font-semibold'>
                     <p className='mx-auto'> Click Here (government authorized center) ⏫</p>
                 </button>
             </div>
